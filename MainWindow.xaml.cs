5c697d350c1b7f4cfb596b056f00825f42e0a74f
joshc0des <joshuahooper3@gmail.com>
2023-01-31 22:23:27 -0600

    completed besides validation

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace WPF_Classes_and_Files
 {
@@ -23,6 +26,54 @@ namespace WPF_Classes_and_Files
         public MainWindow()
         {
             InitializeComponent();
+
+            var rowsOfCSV = File.ReadAllLines("Toys.csv");
+
+            foreach (var item in rowsOfCSV.Skip(1))
+            {
+                Toy t = new Toy();
+                string[] toyData = item.Split(',');
+                t.Manufacturer = toyData[0];
+                t.Name = toyData[1];
+                t.Price = Convert.ToDouble(toyData[2]);
+                t.Image = toyData[3];
+
+                lstToys.Items.Add(t);
+            }
+        }
+
+        private void lstToys_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (lstToys.SelectedItem != null)
+            {
+                Toy selectedToy = (Toy)lstToys.SelectedItem;
+                var uri = new Uri(selectedToy.Image);
+                imageControl.Source = new BitmapImage(uri);
+            } else
+            {
+                imageControl.Source = null;
+            }
+        }
+
+        private void lstToys_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            Toy selectedToy = (Toy)lstToys.SelectedItem;
+
+            if (lstToys.SelectedItem != null)
+            {
+                MessageBox.Show($"{selectedToy.GetAisle()}");
+            }
+        }
+
+        private void subButton_Click(object sender, RoutedEventArgs e)
+        {
+            Toy t = new Toy();
+            t.Manufacturer = txtMan.Text;
+            t.Name = txtNam.Text;
+            t.Price = Convert.ToDouble(txtPri.Text);
+            t.Image = txtImg.Text;
+
+            lstToys.Items.Add(t);
         }
     }
 }
---
+++ Toy.cs
@@ -0,0 +1,35 @@
+﻿namespace WPF_Classes_and_Files
+{
+    internal class Toy
+    {
+        public string Manufacturer { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Image { get; set; }
+        private string Aisle { get; set; }
+
+        public Toy()
+        {
+            Manufacturer = string.Empty;
+            Name = string.Empty;
+            Price = 0;
+            Image = string.Empty;
+            Aisle = string.Empty;
+        }
+
+        public string GetAisle()
+        {
+            string manuf = Manufacturer.Substring(0, 1).ToUpper();
+            string pri = Price.ToString().Replace(".", "");
+            string outstr = manuf + pri;
+
+            return outstr;
+        }
+
+        public override string ToString()
+        {
+            string output = $"{Manufacturer} - {Name}";
+            return output;
+        }
+    }
+}
